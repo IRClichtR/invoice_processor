d10037ec07982bff782f3ba41bdbf32577704e46
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2026-01-27 12:04:42 +0100

    fix dead code pinia ref

--- tauri-app/src/main.ts
+++ tauri-app/src/main.ts
@@ -13,10 +13,7 @@
 // limitations under the License.
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import "./assets/main.css";
 import App from "./App.vue";
 
-const app = createApp(App);
-app.use(createPinia());
-app.mount("#app");
+createApp(App).mount("#app");
